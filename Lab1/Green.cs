3b42584781d327a3977a1d8138a77ad5b7e3540a
Artem739-cnt <egorchev2007@mail.ru>
2025-09-19 10:40:52 +0300

    Add files via upload

--- Lab1/Green.cs
+++ Lab1/Green.cs
@@ -7,7 +7,15 @@
             bool answer = false;
 
             // code here
-
+            double k = Math.Abs(d);
+            if (k >= 1)
+            {
+                answer = true;
+            }
+            else
+            {
+                answer = false;
+            }
             // end
 
             return answer;
@@ -17,27 +25,45 @@
             bool answer = false;
 
             // code here
+            double sr = (d + f) / 2;
+            if (sr > 0)
+            {
+                answer = true;
+            }
+            else
+            {
+                answer = false;
+            }
+                // end
 
-            // end
-
-            return answer;
+                return answer;
         }
         public bool Task3(int a, int b)
         {
             bool answer = false;
 
             // code here
+            int summ = a + b;
+            int Sr = (Math.Abs(a) + Math.Abs(b)) / 2;
+            if (summ > Sr)
+            {
+                answer = true;
+            }
+            else
+            {
+                answer = false;
+            }
+                // end
 
-            // end
-
-            return answer;
+                return answer;
         }
         public int Task4(int a, int b, int c)
         {
             int answer = 0;
 
             // code here
-
+            int Max1 = Math.Max(a, b);
+            int answer = Math.Max(Max1, c);
             // end
 
             return answer;
@@ -47,7 +73,14 @@
             double answer = 0;
 
             // code here
-
+            if (Math.Abs(x) > 1)
+            {
+                answer = 0;
+            }
+            else if (Math.Abs(x) <= 1)
+            {
+                answer = Math.Pow(x,2) - 1;
+            }
             // end
 
             return answer;
@@ -57,7 +90,18 @@
             bool answer = false;
 
             // code here
-
+            if (y < 0)
+            {
+                answer = false;
+            }
+            else if ((x < 0) && (y <= (x + 1)))
+            {
+                answer = true;
+            }
+            else if ((x >= 0) && (y <= (1 - x)))
+            {
+                answer = true;
+            }
             // end
 
             return answer;
@@ -68,17 +112,37 @@
             bool answer = true;
 
             // code here
+            if (n>=0)
+            {
+                if (n%2 == 0)
+                {
+                    answer = false
+                }
+            }
+            else
+            {
+                answer = false
+            }
+                // end
 
-            // end
-
-            return answer;
+                return answer;
         }
         public bool Task8(int X, int Y)
         {
             bool answer = false;
 
             // code here
-
+            int SumY = 0;
+            if (X >= 7)//7 дней нужно минимально, тк встает в 14 а надо в 7 и практика каждый день отнимает час
+            {
+                for (int i = 0; i < X; i++)
+                { 
+                    if ((i % 2) == 0) { SumY += Y; }
+                 
+                }
+                if ((SumY >= 4 * 60) && (SumY <= 6 * 60))//ложится в 4 утра, а нужно в 22 или 24 вечера,тоесть разница 4-6 часов, соответственно нужно чтобы чай покарыл это время, но чай уменьшает на минуты, поэтому 4часа = 4*60 = 240 минут и с 6 аналогично
+                    answer = true;
+            }
             // end
 
             return answer;
