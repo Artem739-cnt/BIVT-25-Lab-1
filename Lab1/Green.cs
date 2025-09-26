10cb939477119103abe947a24f21f4c2c475c2e4
Artem739-cnt <egorchev2007@mail.ru>
2025-09-26 09:01:23 +0300

    Add files via upload

--- Lab1/Green.cs
+++ Lab1/Green.cs
@@ -34,9 +34,9 @@
             {
                 answer = false;
             }
-                // end
+            // end
 
-                return answer;
+            return answer;
         }
         public bool Task3(int a, int b)
         {
@@ -53,9 +53,9 @@
             {
                 answer = false;
             }
-                // end
+            // end
 
-                return answer;
+            return answer;
         }
         public int Task4(int a, int b, int c)
         {
@@ -63,7 +63,7 @@
 
             // code here
             int Max1 = Math.Max(a, b);
-            int answer = Math.Max(Max1, c);
+            answer = Math.Max(Max1, c);
             // end
 
             return answer;
@@ -79,7 +79,7 @@
             }
             else if (Math.Abs(x) <= 1)
             {
-                answer = Math.Pow(x,2) - 1;
+                answer = Math.Pow(x, 2) - 1;
             }
             // end
 
@@ -112,20 +112,20 @@
             bool answer = true;
 
             // code here
-            if (n>=0)
+            if (n >= 0)
             {
-                if (n%2 == 0)
+                if (n % 2 == 0)
                 {
-                    answer = false
+                    answer = false;
                 }
             }
             else
             {
-                answer = false
+                answer = false;
             }
-                // end
+            // end
 
-                return answer;
+            return answer;
         }
         public bool Task8(int X, int Y)
         {
@@ -136,9 +136,9 @@
             if (X >= 7)//7 дней нужно минимально, тк встает в 14 а надо в 7 и практика каждый день отнимает час
             {
                 for (int i = 0; i < X; i++)
-                { 
+                {
                     if ((i % 2) == 0) { SumY += Y; }
-                 
+
                 }
                 if ((SumY >= 4 * 60) && (SumY <= 6 * 60))//ложится в 4 утра, а нужно в 22 или 24 вечера,тоесть разница 4-6 часов, соответственно нужно чтобы чай покарыл это время, но чай уменьшает на минуты, поэтому 4часа = 4*60 = 240 минут и с 6 аналогично
                     answer = true;
